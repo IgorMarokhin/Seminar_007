6971e4cee1e2fefa4d639f260578a0e2c1514831
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-29 23:07:39 +0300

    Семинар 7. Изменено решение задачи 51.

---
+++ Seminar_007_Task_51/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1,1) и т.д.).
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 10);
+int sum = SumDiagonal(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"{sum}");
+
+//////////////////////////////////////////////////
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int SumDiagonal(int [,] inArray)
+{
+    int sum = 0;
+    int size = 0;
+    if (inArray.GetLength(0) < inArray.GetLength(1))
+        size = inArray.GetLength(0);
+    else 
+        size = inArray.GetLength(1);
+    for (int i = 0; i < size; i++)
+    {
+        sum += inArray[i, i];
+    }
+    return sum;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+         for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
