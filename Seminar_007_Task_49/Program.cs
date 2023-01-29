004e64628ee502802ff05fc92fc47e51157642ef
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-29 22:41:33 +0300

    Семинар 7. Добавлено решение задачи 49.

---
+++ Seminar_007_Task_49/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы,
+// у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int[,] array = GetArray(rows, columns, 0, 10);
+int[,] result = ChangeArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(result);
+
+//////////////////////////////////////////////////
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int[,] ChangeArray(int [,] array)
+{
+    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+                result[i, j] = array[i, j] * array[i, j];
+            else
+                result[i, j] = array[i, j];
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+         for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
